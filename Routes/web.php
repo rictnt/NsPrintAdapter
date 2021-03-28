cde2643cfbe20b5553b3a77b53e272ae9ac168f6
Blair2004 <carlosjohnsonluv2004@gmail.com>
2021-03-28 01:54:43 +0100

    Update

--- Routes/web.php
+++ Routes/web.php
@@ -5,9 +5,11 @@ use App\Http\Middleware\Authenticate;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware([
-    SubstituteBindings::class,
-    Authenticate::class
-])->group( function() {
-    Route::get( '/print-adapter/settings', [ NsPrintAdapterController::class, 'getSettingsPage' ]);
+Route::prefix( 'dashboard' )->group( function() {
+    Route::middleware([
+        SubstituteBindings::class,
+        Authenticate::class
+    ])->group( function() {
+        Route::get( '/print-adapter/settings', [ NsPrintAdapterController::class, 'getSettingsPage' ]);
+    });
 });
