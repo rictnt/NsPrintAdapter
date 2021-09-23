8bf88378a9613d19ea453768395cec520da22286
blair2004 <carlosjohnsonluv2004@gmail.com>
2021-09-23 19:45:41 +0000

    Changelog - Fixed: unable to print to the order from Gastro tables

--- Routes/api.php
+++ Routes/api.php
@@ -7,5 +7,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([
     SubstituteBindings::class
 ])->group( function() {
-    Route::get( '/nexopos/v4/ns-printadapter/{order}/print-data/', [ NsPrintAdapterController::class, 'getReceipt' ]);
+    Route::get( '/ns-printadapter/{order}/print-data/', [ NsPrintAdapterController::class, 'getReceipt' ]);
 });
